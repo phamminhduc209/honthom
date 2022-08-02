75c3f5e6c705587777d6a74465e066535611e427
DucPM <minhducpham.it@gmail.com>
2022-08-02 20:26:40 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var app = app || {};
-
-app.init = function () {};
-
-$(document).ready(function () {
-	app.init();
-	var wow = new WOW({
-		animateClass: "animate__animated",
-	});
-	wow.init();
+var $reveal = $(".img-pos");
+$reveal.draggable({
+    scroll: true,
+    containment: "#container",
+    snap: '.gridlines',
+    stop: function () {
+        var l = (100 * parseFloat($(this).position().left / parseFloat($(this).parent().width()))) + "%";
+        var t = (100 * parseFloat($(this).position().top / parseFloat($(this).parent().height()))) + "%";
+        $(this).css("left", l);
+        $(this).css("top", t);
+    }
 });
