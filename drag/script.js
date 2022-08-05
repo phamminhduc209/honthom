42690505ee9e14e40121e0aa2705b5198155f8d6
phamminhduc209 <minhducpham.it@gmail.com>
2022-08-05 16:45:13 +0700

    modal

--- drag/script.js
+++ drag/script.js
@@ -1,13 +1,17 @@
 var $reveal = $(".img-pos");
 
-$reveal.draggable({
-    scroll: true,
-    containment: "#container",
-    snap: '.gridlines',
-    stop: function () {
-        var l = (100 * parseFloat($(this).position().left / parseFloat($(this).parent().width()))) + "%";
-        var t = (100 * parseFloat($(this).position().top / parseFloat($(this).parent().height()))) + "%";
-        $(this).css("left", l);
-        $(this).css("top", t);
-    }
-});
+function rev() = {
+    $reveal.draggable({
+        scroll: true,
+        containment: "#container",
+        snap: '.gridlines',
+        stop: function () {
+            var l = (100 * parseFloat($(this).position().left / parseFloat($(this).parent().width()))) + "%";
+            var t = (100 * parseFloat($(this).position().top / parseFloat($(this).parent().height()))) + "%";
+            $(this).css("left", l);
+            $(this).css("top", t);
+        }
+    });
+    
+}
+
